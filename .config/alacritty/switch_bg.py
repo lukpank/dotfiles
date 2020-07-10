caf2ff3f7ef2ee6aa226b062f7c7b0a5084b1530
Łukasz Pankowski <lukpank@o2.pl>
2020-07-10 23:35:11 +0200

    alacritty: dark green and light mint colors

--- .config/alacritty/switch_bg.py
+++ .config/alacritty/switch_bg.py
@@ -5,11 +5,11 @@ import os
 path = os.path.join(os.getenv('HOME'), '.config/alacritty/alacritty.yml')
 with open(path, 'r+') as f:
     cfg = f.read()
-    if "    background: '#424242'" in cfg:
-        cfg = cfg.replace("    background: '#424242'", "    background: '#fafafa'")
-        cfg = cfg.replace("    foreground: '#eeeeee'", "    foreground: '#424242'")
+    if "    background: '#1a3a34'" in cfg:
+        cfg = cfg.replace("    background: '#1a3a34'", "    background: '#e2f6e1'")
+        cfg = cfg.replace("    foreground: '#e2f6e1'", "    foreground: '#1a3a34'")
     else:
-        cfg = cfg.replace("    background: '#fafafa'", "    background: '#424242'")
-        cfg = cfg.replace("    foreground: '#424242'", "    foreground: '#eeeeee'")
+        cfg = cfg.replace("    background: '#e2f6e1'", "    background: '#1a3a34'")
+        cfg = cfg.replace("    foreground: '#1a3a34'", "    foreground: '#e2f6e1'")
     f.seek(0)
     f.write(cfg)
