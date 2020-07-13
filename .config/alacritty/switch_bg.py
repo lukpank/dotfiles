0aaad0a08fb2fd4c9c10927f3d865b8d9e7baf54
Łukasz Pankowski <lukpank@o2.pl>
2020-07-13 22:53:37 +0200

    less intensive light and urgent color; restrictive FIFO mode

--- .config/alacritty/switch_bg.py
+++ .config/alacritty/switch_bg.py
@@ -6,10 +6,10 @@ path = os.path.join(os.getenv('HOME'), '.config/alacritty/alacritty.yml')
 with open(path, 'r+') as f:
     cfg = f.read()
     if "    background: '#1a3a34'" in cfg:
-        cfg = cfg.replace("    background: '#1a3a34'", "    background: '#e2f6e1'")
-        cfg = cfg.replace("    foreground: '#e2f6e1'", "    foreground: '#1a3a34'")
+        cfg = cfg.replace("    background: '#1a3a34'", "    background: '#f2f6e1'")
+        cfg = cfg.replace("    foreground: '#f2f6e1'", "    foreground: '#1a3a34'")
     else:
-        cfg = cfg.replace("    background: '#e2f6e1'", "    background: '#1a3a34'")
-        cfg = cfg.replace("    foreground: '#1a3a34'", "    foreground: '#e2f6e1'")
+        cfg = cfg.replace("    background: '#f2f6e1'", "    background: '#1a3a34'")
+        cfg = cfg.replace("    foreground: '#1a3a34'", "    foreground: '#f2f6e1'")
     f.seek(0)
     f.write(cfg)
