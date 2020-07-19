d81399a25ea9219e13880af8b3f1ddec0cc10497
Łukasz Pankowski <lukpank@o2.pl>
2020-07-19 23:36:54 +0200

    alacritty: use my own dark and light color schemes

--- .config/alacritty/switch_bg.py
+++ .config/alacritty/switch_bg.py
@@ -5,11 +5,9 @@ import os
 path = os.path.join(os.getenv('HOME'), '.config/alacritty/alacritty.yml')
 with open(path, 'r+') as f:
     cfg = f.read()
-    if "    background: '#1a3a34'" in cfg:
-        cfg = cfg.replace("    background: '#1a3a34'", "    background: '#f2f6e1'")
-        cfg = cfg.replace("    foreground: '#f2f6e1'", "    foreground: '#1a3a34'")
+    if "colors:  *dark" in cfg:
+        cfg = cfg.replace("colors:  *dark", "colors: *light")
     else:
-        cfg = cfg.replace("    background: '#f2f6e1'", "    background: '#1a3a34'")
-        cfg = cfg.replace("    foreground: '#1a3a34'", "    foreground: '#f2f6e1'")
+        cfg = cfg.replace("colors: *light", "colors:  *dark")
     f.seek(0)
     f.write(cfg)
