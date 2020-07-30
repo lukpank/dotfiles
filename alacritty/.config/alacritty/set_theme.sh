fa4d56a6f4d56f0b58bdde85ee893589a1d29d64
Łukasz Pankowski <lukpank@o2.pl>
2020-07-30 13:39:54 +0200

    alacritty: add theme prefix and new themes to set_theme.sh

--- alacritty/.config/alacritty/set_theme.sh
+++ alacritty/.config/alacritty/set_theme.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
 case "$1" in
-    dark|dark-blue|light)
+    lupan-dark|lupan-dark-blue|lupan-dark-gray|lupan-light|lupan-material-dark|lupan-material-light)
 	sed -i "s/^colors: [*].*/colors: *$1/" ~/.config/alacritty/alacritty.yml
 	;;
-    next)
+    --next)
 	case $(grep colors: ~/.config/alacritty/alacritty.yml) in
-	    'colors: *dark')
-		THEME=dark-blue
+	    'colors: *lupan-dark')
+		THEME=lupan-dark-blue
 		;;
-	    'colors: *dark-blue')
-		THEME=light
+	    'colors: *lupan-dark-blue')
+		THEME=lupan-dark-gray
+		;;
+	    'colors: *lupan-dark-gray')
+		THEME=lupan-light
+		;;
+	    'colors: *lupan-light')
+		THEME=lupan-material-dark
+		;;
+	    'colors: *lupan-material-dark')
+		THEME=lupan-material-light
 		;;
 	    *)
-		THEME=dark
+		THEME=lupan-dark
 		;;
 	esac
 	sed -i "s/^colors: [*].*/colors: *$THEME/" ~/.config/alacritty/alacritty.yml
