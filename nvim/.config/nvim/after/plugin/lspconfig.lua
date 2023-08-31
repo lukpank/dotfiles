fc8d940856fa8dc9cde5a85ad34ef3d749604bf3
Łukasz Pankowski <lukpank@o2.pl>
2023-09-01 01:44:57 +0200

    cmp C-l confirm, no autocomplete

--- nvim/.config/nvim/after/plugin/lspconfig.lua
+++ nvim/.config/nvim/after/plugin/lspconfig.lua
@@ -85,12 +85,14 @@ require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
 
 cmp.setup {
+  completion = { autocomplete = false },
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
     end,
   },
   mapping = cmp.mapping.preset.insert {
+    ['<C-l>'] = cmp.mapping.confirm({ select = true }),
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
