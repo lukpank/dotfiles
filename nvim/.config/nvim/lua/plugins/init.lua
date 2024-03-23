a84dac1b3d2e439c99864168a91784b50364c955
Łukasz Pankowski <lukpank@o2.pl>
2024-03-24 00:06:22 +0100

    nvim: add leap

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -4,4 +4,15 @@ return {
     dependencies = 'neovim/nvim-lspconfig',
     lazy = true,
   },
+
+  {
+    'ggandor/leap.nvim',
+    config = function()
+      require('leap').add_default_mappings()
+    end,
+    dependencies = {
+      "tpope/vim-repeat",
+    },
+    lazy = false,
+  },
 }
