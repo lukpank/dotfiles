0c0a1a9f9c46055b550b700246b7284b9946812d
Łukasz Pankowski <lukpank@o2.pl>
2024-06-15 16:07:34 +0200

    nvim: change harpoon keys

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -9,9 +9,12 @@ return {
       vim.keymap.set('n', '<leader>i', function() harpoon:list():add() end, { desc = "Harpoon add new [I]tem" })
       vim.keymap.set('n', '<leader>m', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
         { desc = "Harpoon [M]enu" })
-      for i = 1, 9, 1 do
-        vim.keymap.set('n', '<M-' .. i .. '>', function() harpoon:list():select(i) end)
-      end
+      vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end)
+      vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end)
+      vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end)
+      vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end)
+      vim.keymap.set('n', '<leader>5', function() harpoon:list():prev() end)
+      vim.keymap.set('n', '<leader>6', function() harpoon:list():next() end)
     end
   },
 }
