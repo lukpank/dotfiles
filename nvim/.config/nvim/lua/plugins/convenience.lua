5b421c7999c32b2a218a277254a65a596f1026e8
Łukasz Pankowski <lukpank@o2.pl>
2024-08-04 23:02:15 +0200

    nvim: update which-key naming groups to new format

--- nvim/.config/nvim/lua/plugins/convenience.lua
+++ nvim/.config/nvim/lua/plugins/convenience.lua
@@ -6,13 +6,13 @@ return {
       require('which-key').setup(opts)
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode/Color', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { "<leader>c", group = "[C]ode/Color" },
+        { "<leader>d", group = "[D]ocument" },
+        { "<leader>g", group = "[G]it" },
+        { "<leader>r", group = "[R]ename" },
+        { "<leader>s", group = "[S]earch" },
+        { "<leader>w", group = "[W]orkspace" },
       }
     end,
   },
