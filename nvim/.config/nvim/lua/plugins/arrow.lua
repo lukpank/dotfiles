24966cfc69508d1a5479430e50b083915daafa3f
Łukasz Pankowski <lukpank@o2.pl>
2025-03-16 23:23:00 +0100

    nvim: change arrow leader key

--- nvim/.config/nvim/lua/plugins/arrow.lua
+++ nvim/.config/nvim/lua/plugins/arrow.lua
@@ -3,7 +3,7 @@ return {
     "otavioschwanck/arrow.nvim",
     opts = {
       show_icons = true,
-      leader_key = '<leader>;',
+      leader_key = '-',
       buffer_leader_key = '<leader>m',
     },
     keys = {
