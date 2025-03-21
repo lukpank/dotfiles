f685ebee2c4c91bc725b95b0ed9724c4dace7c1d
Łukasz Pankowski <lukpank@o2.pl>
2025-03-21 02:17:25 +0100

    nvim: notimeout

--- nvim/.config/nvim/lua/lupan/options.lua
+++ nvim/.config/nvim/lua/lupan/options.lua
@@ -31,7 +31,7 @@ vim.wo.signcolumn = 'yes'
 
 -- Decrease update time
 vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+vim.o.timeout = false
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
