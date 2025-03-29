d3b8e76787ddd23fd5a1d7b4e9fd3dc8574dc619
Łukasz Pankowski <lukpank@o2.pl>
2025-03-29 12:38:03 +0100

    nvim: add zen-mode plugin
    toggles hyperland and awesome fullscreen mode

--- nvim/.config/nvim/lua/plugins/convenience.lua
+++ nvim/.config/nvim/lua/plugins/convenience.lua
@@ -22,4 +22,26 @@ return {
       require("better_escape").setup(opts)
     end,
   },
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      window = { width = 180 },
+      plugins = {
+        tmux = { enabled = true },
+      },
+      on_open = function()
+        vim.opt.laststatus = 1
+        require('incline').disable()
+        vim.system({ 'hyprctl', 'dispatch', 'fullscreen' })
+        vim.system({ 'awesome-client', 'client.focus.fullscreen = true' })
+      end,
+      on_close = function()
+        vim.opt.laststatus = 3
+        require('incline').enable()
+        vim.system({ 'hyprctl', 'dispatch', 'fullscreen' })
+        vim.system({ 'awesome-client', 'client.focus.fullscreen = false' })
+      end
+    },
+    keys = { { "<leader>z", "<cmd>ZenMode<CR>" } },
+  },
 }
