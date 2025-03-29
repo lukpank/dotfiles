cfb882fdc9926688c93d203125e5b5febaa0ce7a
Łukasz Pankowski <lukpank@o2.pl>
2025-03-29 12:23:51 +0100

    nvim: auto switch light/dark background

--- nvim/.config/nvim/lua/lupan/utils.lua
+++ nvim/.config/nvim/lua/lupan/utils.lua
@@ -1,7 +1,9 @@
 local M = {}
 
+local filename = os.getenv('HOME') .. '/.config/alacritty/alacritty.toml'
+
 function M.terminalbg()
-  local ok, lines = pcall(io.lines, os.getenv('HOME') .. '/.config/alacritty/alacritty.toml')
+  local ok, lines = pcall(io.lines, filename)
   if ok then
     for line in lines do
       if string.find(line, 'light.toml') then
@@ -11,5 +13,17 @@ function M.terminalbg()
   end
   return "dark"
 end
+
+local w = vim.uv.new_fs_event()
+
+local function watch(fname)
+  w:start(filename, {}, vim.schedule_wrap(function(...)
+    vim.o.background = M.terminalbg()
+    w:stop()
+    watch(fname)
+  end))
+end
+
+watch(filename)
 
 return M
